b2d050311309380a73148c749f9e43c371c33707
Andri Nur Rochman <ceoandri@gmail.com>
2023-05-12 11:07:38 +0700

    add sample animation with menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import calendar from './calendar.svg';
+import userGroup from './user-group.svg';
 import logoCollapse from './logo-collapse.svg';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 import { Button, Tooltip } from 'antd';
@@ -36,6 +38,16 @@ const App = () => {
       <div className={`sidebar ${sidebarHandlerClass}`}>
         <div className="logo">
           <img className={isExpand ? 'logo-expand' : 'logo-collapse'} src={logoUrl} alt="logo"></img>
+        </div>
+        <div className="menu">
+          <div className="menu-item active">
+            <img className="menu-icon" src={userGroup}></img>
+            <label className={isExpand ? 'menu-label label-expand' : 'menu-label label-collapse'}>People & Users</label>
+          </div>
+          <div className="menu-item">
+            <img className="menu-icon" src={calendar}></img>
+            <label className={isExpand ? 'menu-label label-expand' : 'menu-label label-collapse'}>Calendar Setting</label>
+          </div>
         </div>
       </div>
       <div className="content">
